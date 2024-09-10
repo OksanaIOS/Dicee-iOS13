0feaf1c750fb447fcc86d0a05cbffd14b1d7b719
Oksana ogay <example@email.com>
2024-09-10 15:24:32 +0500

    Create cubeImage

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+        let imageView = UIImageView()
+        let logoImageView = UIImageView()
+        let cubeLeft = UIImageView()
+        let cubeRight = UIImageView()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Установка фона
+            setupImageView(imageView, frame: CGRect(x: -10, y: 0, width: 424, height: 891), imageName: "GreenBackground.png", contentMode: .scaleAspectFill)
+            
+            // Установка логотипа
+            setupImageView(logoImageView, frame: CGRect(x: 80, y: 175, width: 240, height: 128), imageName: "DiceeLogo.png")
+            
+            // Установка левого кубика
+            setupImageView(cubeLeft, frame: CGRect(x: 55, y: 417, width: 120, height: 120), imageName: "DiceOne.png")
+            
+            // Установка правого кубика
+            setupImageView(cubeRight, frame: CGRect(x: 220, y: 417, width: 120, height: 120), imageName: "DiceTwo.png")
+        }
+        
+        // Функция для настройки imageView
+        func setupImageView(_ imageView: UIImageView, frame: CGRect, imageName: String, contentMode: UIView.ContentMode = .scaleAspectFit) {
+            imageView.frame = frame
+            imageView.image = UIImage(named: imageName)
+            imageView.contentMode = contentMode
+            view.addSubview(imageView)
+        }
     }
 
 
-}
 
