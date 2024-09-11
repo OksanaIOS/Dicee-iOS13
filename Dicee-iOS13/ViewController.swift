0b185ac039824672286356ab0dfe75437086d1fd
Oksana ogay <example@email.com>
2024-09-11 14:20:53 +0500

    Button and random image

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,45 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-//    
-//        let imageView = UIImageView()
-//        let logoImageView = UIImageView()
-//        let cubeLeft = UIImageView()
-//        let cubeRight = UIImageView()
-      
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-//            // Установка фона
-//            setupImageView(imageView, frame: CGRect(x: -10, y: 0, width: 424, height: 891), imageName: "GreenBackground.png", contentMode: .scaleAspectFill)
-//            
-//            // Установка логотипа
-//            setupImageView(logoImageView, frame: CGRect(x: 80, y: 175, width: 240, height: 128), imageName: "DiceeLogo.png")
-//            
-//            // Установка левого кубика
-//            setupImageView(cubeLeft, frame: CGRect(x: 55, y: 417, width: 120, height: 120), imageName: "DiceOne.png")
-//            
-//            // Установка правого кубика
-//            setupImageView(cubeRight, frame: CGRect(x: 220, y: 417, width: 120, height: 120), imageName: "DiceTwo.png")
-//            
-//          
-//
-//        }
-//    
-//        
-//        // Функция для настройки imageView
-//        func setupImageView(_ imageView: UIImageView, frame: CGRect, imageName: String, contentMode: UIView.ContentMode = .scaleAspectFit) {
-//            imageView.frame = frame
-//            imageView.image = UIImage(named: imageName)
-//            imageView.contentMode = contentMode
-//            view.addSubview(imageView)
-        }
-//    
     
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
+    var diceArray: [UIImage?] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        Создаем массив со всеми кубиками
+        diceArray = [ UIImage(named: "DiceOne"),
+                          UIImage(named: "DiceTwo"),
+                          UIImage(named: "DiceThree"),
+                          UIImage(named: "DiceFour"),
+                          UIImage(named: "DiceFive"),
+                          UIImage(named: "DiceSix")]
     }
-
+        
+//      При нажатии на кнопку выдается рандомное изображение из массива
+        @IBAction func rollButtonPressed(_ sender: UIButton) {
+            diceImageView1.image = diceArray[Int.random(in: 0...5)]
+            diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+}
 
 
